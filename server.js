0fde993d7cc1e269152f81fd1286f04466e6c6f8
Nick Rose <nickfbp@gmail.com>
2021-01-20 19:50:06 -0700

    Morgan

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3001;
 
 const app = express();
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'));
 
 app.use((req, res) => {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>This is and Express Server</h1></body></html>');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 });
 
 app.listen(port, hostname, () => {
